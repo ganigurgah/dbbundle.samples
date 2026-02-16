9827b1eb02a4867e6203c75a972875db7ef6d19b
Gani GÜRGAH <ganigurgah@gmail.com>
2026-02-16 21:57:03 +0300

    remove old files

--- src/main/java/com/valora/test/repository/SystemConfigRepository.java
+++
@@ -1,8 +0,0 @@
-package com.valora.test.repository;
-
-import com.valora.test.entity.SystemConfigEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SystemConfigRepository extends JpaRepository<SystemConfigEntity, String> {
-    SystemConfigEntity findByConfigKey(String configKey);
-}
